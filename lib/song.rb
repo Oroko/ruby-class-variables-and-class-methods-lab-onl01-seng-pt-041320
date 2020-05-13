f30e4e9351ec55c1452472eb8f21fa1a08e55e6d
Abel Gechure <aoroko09@gmail.com>
2020-05-13 18:39:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,16 +41,16 @@ class Song
      genre_count
   end
   
-  def self.artists_count
+  def self.artist_count
     atrist_count = {}
     @@artists.each do |artist|
-      if artists_count[artist] 
-        artists_count[artist] += 1 
+      if artist_count[artist] 
+        artist_count[artist] += 1 
       else
-        artists_count[artist] = 1
+        artist_count[artist] = 1
       end
     end 
-    artists_count
+    artist_count
   end
   
 end
