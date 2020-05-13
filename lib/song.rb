6a400dffe0eb5cc58321b8d8973b08965aaed2f5
Abel Gechure <aoroko09@gmail.com>
2020-05-13 18:20:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,12 +31,12 @@ class Song
   end
   def self.genre_count
     @@genres.each do |genre|
-      if @@genre_count[genre] 
-        @@genre_count[genre] += 1
+      if genre_count[genre] 
+        genre_count[genre] += 1
       else
-        @@genre_count[genre] = 1
+        genre_count[genre] = 1
       end
-      @@genre_count
+      genre_count
   end
   
 end
