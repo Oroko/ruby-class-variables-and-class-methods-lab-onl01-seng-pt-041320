02dd97c1f307e9f831f0eadc0b07ccee13004047
Abel Gechure <aoroko09@gmail.com>
2020-05-13 17:19:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song 
   
   @@count = 0
-  @@artists 
+  @@artists = [] 
   attr_accessor :name, :artist, :genre
   
   def initialize(name,artist,genre)
@@ -9,6 +9,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists.push
     
   end
   
