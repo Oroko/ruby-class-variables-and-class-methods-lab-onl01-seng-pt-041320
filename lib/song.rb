15325a07af635c8550a0b29f588a42cb09ad644d
Abel Gechure <aoroko09@gmail.com>
2020-05-13 17:17:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+  
+  @@count = 0
+  @@artists 
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  # def self.count
+  #   @@artists
+  # end
+  
+  
+end
